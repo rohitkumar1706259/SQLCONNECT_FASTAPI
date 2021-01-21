9b3cecf4d4890dae84773a2bf176688cdb470afa
rohitkumar1706259 <54408085+rohitkumar1706259@users.noreply.github.com>
2021-01-21 19:18:46 +0530

    Add files via upload

---
+++ sql_connection/__init__.py
@@ -0,0 +1,11 @@
+'''
+
+
+import configparser
+#file='config.ini'
+config=configparser.ConfigParser()
+config.read(r'config.ini')
+database_type=config.get('mount','database_type')
+print(database_type)
+'''
+
